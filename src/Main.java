7732f6af984f47d40eaf1ba39257c277b039c26b
vojtech.hampl <v.hampl31@gmail.com>
2023-02-20 20:52:09 +0100

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //pro cyklus s opakováním 100, kdy je přičítána hodnota 1 od 1 do 1OO
+        for (int i = 1; i <= 100; i++) {
+            //je v případě, že je hodnota z cyklu dělitelná třemi i 5, je vypsána hodnota FIZZBUZZ
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FIZZBUZZ");
+            }
+            //je v případě, že je hodnota z cyklu dělitelná třemi, je vypsána hodnota FIZZ
+            else if (i % 3 == 0) {
+                System.out.println("FIZZ");
+            }
+            //je v případě, že je hodnota z cyklu dělitelná pěti, je vypsána hodnota BUZZ
+            else if (i % 5 == 0) {
+                System.out.println("BUZZ");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
